d63635aadddf1b308d3915d1615fbd591fc55e23
Valentin Manes <valentinmanes@outlook.fr>
2022-06-01 08:25:52 +0200

    Add init probability and balance

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -6,8 +6,17 @@ contract Market {
   address public owner;
   string public name;
 
-  constructor(string memory _name) payable {
+  uint public constant maxProb = 10000;
+  uint public initProbability;
+
+  constructor(string memory _name, uint probability) payable {
     owner = msg.sender;
     name = _name;
+
+    require(probability > 0, "Need a strictly positive initial probability");
+    require(probability < maxProb, "Probability range from 0 to 10000");
+    initProbability = probability;
+
+    require(msg.value > 0, "Need liquidity to be initialized");
   }
 }
--- test/market.ts
+++ test/market.ts
@@ -2,11 +2,13 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Market", () => {
-  it("should do correctly set the address and name", async () => {
-    const mockName = "First market name";
+  const mockName = "First market name";
+  const mockProb = 100;
+  const oneEth = ethers.utils.parseEther("1.0");
 
+  it("should do correctly set the address and name", async () => {
     const Market = await ethers.getContractFactory("Market");
-    const market = await Market.deploy(mockName);
+    const market = await Market.deploy(mockName, mockProb, { value: oneEth });
     const [owner] = await ethers.getSigners();
 
     await market.deployed();
@@ -14,21 +16,32 @@ describe("Market", () => {
     expect(await market.owner()).to.equal(owner.address);
   });
 
-  it("should have zero balance", async () => {
-    const Market = await ethers.getContractFactory("Market");
-    const market = await Market.deploy("");
-    await market.deployed();
-
-    expect(await ethers.provider.getBalance(market.address)).to.equal(0);
-  });
-
   it("should have the initialized balance", async () => {
-    const oneEth = ethers.utils.parseEther("1.0");
-
     const Market = await ethers.getContractFactory("Market");
-    const market = await Market.deploy("", { value: oneEth });
+    const market = await Market.deploy("", mockProb, { value: oneEth });
 
     await market.deployed();
     expect(await ethers.provider.getBalance(market.address)).to.equal(oneEth);
+  });
+
+  it("should fail if the probably is too low", async () => {
+    const Market = await ethers.getContractFactory("Market");
+    await expect(Market.deploy(mockName, 0)).to.be.revertedWith(
+      "Need a strictly positive initial probability"
+    );
+  });
+
+  it("should fail if the probably is too high", async () => {
+    const Market = await ethers.getContractFactory("Market");
+    await expect(Market.deploy(mockName, 10000)).to.be.revertedWith(
+      "Probability range from 0 to 10000"
+    );
+  });
+
+  it("should fail if the probably is too high", async () => {
+    const Market = await ethers.getContractFactory("Market");
+    await expect(Market.deploy(mockName, mockProb)).to.be.revertedWith(
+      "Need liquidity to be initialized"
+    );
   });
 });
