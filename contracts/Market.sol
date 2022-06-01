6c88fcad5cb55d59d401744669eb6fecaa7d998f
Valentin Manes <valentinmanes@outlook.fr>
2022-06-01 19:39:08 +0200

    Init ERC 20 Yes and No token in contract

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -1,16 +1,20 @@
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
+import "./Token.sol";
 
-contract Market {
-  address public owner;
+/// @custom:security-contact valentin@invisoo.com
+contract Market is Ownable {
   string public name;
 
   uint public constant maxProb = 10000;
   uint public initProbability;
 
+  YesToken yesToken;
+  NoToken noToken;
+
   constructor(string memory _name, uint probability) payable {
-    owner = msg.sender;
     name = _name;
 
     require(probability > 0, "Need a strictly positive initial probability");
@@ -18,5 +22,8 @@ contract Market {
     initProbability = probability;
 
     require(msg.value > 0, "Need liquidity to be initialized");
+    uint initFund = msg.value;
+    yesToken = new YesToken(initFund);
+    noToken = new NoToken(initFund);
   }
 }
---
+++ contracts/Token.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact valentin@invisoo.com
+contract PredictionToken is ERC20, Ownable {
+  constructor(
+    string memory name_,
+    string memory symbol_,
+    uint256 initFund
+  ) ERC20(name_, symbol_) {
+    _mint(msg.sender, initFund);
+  }
+
+  function mint(address to, uint256 amount) public onlyOwner {
+    _mint(to, amount);
+  }
+
+  function burnAll() public onlyOwner {
+    // @TODO: List all accounts with all amount and burn them with the _burn
+    // function.
+  }
+}
+
+contract YesToken is PredictionToken {
+  constructor(uint256 initFund) PredictionToken("Yes", "YES", initFund) {}
+}
+
+contract NoToken is PredictionToken {
+  constructor(uint256 initFund) PredictionToken("No", "NO", initFund) {}
+}
