22c583ca0c43305f1ef3647fcfaa977dabff49ff
Valentin Manes <valentinmanes@outlook.fr>
2022-06-03 16:33:11 +0200

    Adding Wagmi
    And getting crazy because cannot make the `.bet` call to the contract.

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
 
 import MarketContract from "artifacts/contracts/Market.sol/Market.json";
 
 const contractAddress = process.env.NEXT_PUBLIC_LOCAL_CONTRACT as string;
-// const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID;
 
 interface Market {
   name: string;
@@ -26,28 +26,57 @@ const createContract = () => {
 };
 
 const Home = () => {
+  const { data: account } = useAccount();
+  const { connect, connectors, error, isConnecting, pendingConnector } =
+    useConnect();
+  const { disconnect } = useDisconnect();
+
   const [market, setMarket] = useState<Market | undefined>(undefined);
   const [betSize, setBetSize] = useState<number | undefined>(undefined);
 
-  useEffect(() => {
+  const getInfo = async () => {
+    console.log("fofofof");
     const { provider, contract } = createContract();
 
-    const getInfo = async () => {
-      const balance = await provider.getBalance(contractAddress);
-      const name = await contract.name();
-      const probabilityBN = await contract.impliedProbability();
-      const probability = Number(ethers.utils.formatEther(probabilityBN));
+    const balance = await provider.getBalance(contractAddress);
+    const name = await contract.name();
+    const probabilityBN = await contract.impliedProbability();
+    const probability = Number(ethers.utils.formatEther(probabilityBN));
 
-      setMarket({
-        name: name,
-        balance: ethers.utils.formatEther(balance),
-        probability: probability,
-        contract: contract,
-      });
-    };
+    setMarket({
+      name: name,
+      balance: ethers.utils.formatEther(balance),
+      probability: probability,
+      contract: contract,
+    });
+  };
 
+  useEffect(() => {
     getInfo();
-  }, []);
+  }, [account]);
+
+  if (!account) {
+    return (
+      <div>
+        {connectors.map((connector) => (
+          <button
+            className="btn"
+            disabled={!connector.ready}
+            key={connector.id}
+            onClick={() => connect(connector)}
+          >
+            {connector.name}
+            {!connector.ready && " (unsupported)"}
+            {isConnecting &&
+              connector.id === pendingConnector?.id &&
+              " (connecting)"}
+          </button>
+        ))}
+
+        {error && <div>{error.message}</div>}
+      </div>
+    );
+  }
 
   if (!market)
     return <h1 className="text-5xl font-bold">No market deployed</h1>;
@@ -59,6 +88,17 @@ const Home = () => {
         <p className="py-2">Question: {`"${market.name}"`}</p>
         <p className="py-2">{market.probability * 100}% Chance</p>
         <p className="py-2">Market Asset: {market.balance}</p>
+      </div>
+
+      <div>
+        <div>Account: {account.address}</div>
+        <p>Contract addr: {contractAddress}</p>
+        <button className="btn my-4" onClick={() => disconnect()}>
+          Disconnect
+        </button>
+        <button className="btn my-4" onClick={async () => await getInfo()}>
+          Reload
+        </button>
       </div>
 
       <div className="rounded-xl shadow-xl py-6 px-6 betbox">
@@ -82,9 +122,20 @@ const Home = () => {
           className="mx-4 btn btn-lg btn-outline btn-success"
           onClick={async () => {
             if (!betSize) return;
-            await market.contract.bet(true, {
-              value: ethers.utils.parseEther(betSize.toString()),
-            });
+
+            const provider = new ethers.providers.Web3Provider(
+              (window as any).ethereum
+            );
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(
+              contractAddress,
+              MarketContract.abi,
+              signer
+            );
+
+            const betSizeEth = ethers.utils.parseEther(betSize.toString());
+            // await contract.testF(); // { value: betSizeEth });
+            await contract.bet(true, { value: betSizeEth });
           }}
         >
           Bet Yes
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { WagmiConfig } from "wagmi";
+
+import { client } from "../utils/provider";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />;
+    </WagmiConfig>
+  );
 }
 
 export default MyApp;
