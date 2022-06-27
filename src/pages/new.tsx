4579bbeff1806a579418861fb219104f171c7f85
Valentin Manes <valentinmanes@outlook.fr>
2022-06-27 07:20:37 +0200

    Minor start "new contract" page

---
+++ src/pages/new.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const NewMarket: NextPage = () => {
+  return (
+    <article className="prose">
+      <h1>Make a new Market</h1>
+    </article>
+  );
+};
+
+export default NewMarket;
