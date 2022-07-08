839863de12f9ad94f18e67b4b29702ab92138e36
Valentin Manes <valentinmanes@outlook.fr>
2022-07-08 07:42:48 +0200

    Better looking summary card

--- src/components/summaryCard.tsx
+++ src/components/summaryCard.tsx
@@ -1,6 +1,11 @@
 import { useIsContractLive, useMarketInfos, useReadMarket } from "src/hooks";
 import { Market } from "src/models/market";
 
+const formatProb = (probStr: string | undefined): string => {
+  const prob = Math.round(Number(probStr) * 100);
+  return prob.toString() + " %";
+};
+
 export const SummaryCard = ({ market }: { market: Market }) => {
   const isLive = useIsContractLive(market.address);
   const marketInfo: any = useMarketInfos(market.address);
@@ -20,14 +25,22 @@ export const SummaryCard = ({ market }: { market: Market }) => {
       </div>
     );
 
+  const prob = formatProb(marketInfo.probability);
+
   return (
     <div className="rounded-lg shadow-lg py-6 my-4 bg-base-100">
-      <p>Name: {marketInfo.name}</p>
-      <p>Probability: {marketInfo.probability}</p>
-      <p>Volume: {marketInfo.yesTokenTotSupply}</p>
-      <p>
-        Is Resolved? {"=>"} {isResolved?.toString()}
-      </p>
+      <div className="flex flex-row px-4">
+        <div className="basis-5/6">
+          <p className="text-xl font-bold">{marketInfo.name}</p>
+          <p>Volume: {marketInfo.volume}</p>
+          {isResolved && (
+            <div className="badge badge-primary mx-auto mt-2">Resolved</div>
+          )}
+        </div>
+        <div className="my-auto">
+          <p className="text-3xl font-bold">{prob}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/hooks/contractRead.ts
+++ src/hooks/contractRead.ts
@@ -1,13 +1,22 @@
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import { useAccount, useContractRead } from "wagmi";
+import { useAccount, useBalance, useContractRead } from "wagmi";
 
 import { Market } from "src/components/contractInfo/market";
 import MarketContract from "artifacts/contracts/Market.sol/Market.json";
 
+const formatBet = (probStr: string | undefined): string => {
+  const prob = Math.round(Number(probStr) * 10) / 10;
+  return prob.toString();
+};
+
 export const useMarketInfos = (contractAddr: string): Market | undefined => {
   const [market, setMarket] = useState<Market | undefined>(undefined);
   const { data: account } = useAccount();
+  const { data: balanceData } = useBalance({
+    addressOrName: contractAddr,
+    watch: true,
+  });
 
   const name = useReadMarket(contractAddr, "name");
   const impliedProb = useReadMarket(contractAddr, "impliedProbability");
@@ -15,6 +24,7 @@ export const useMarketInfos = (contractAddr: string): Market | undefined => {
   const userBalance = useReadMarket(contractAddr, "tokenBalanceOf", [
     account?.address,
   ]);
+  const volume = `${formatBet(balanceData?.formatted)} ${balanceData?.symbol}`;
 
   useEffect(() => {
     if (!name || !impliedProb || !totalSupply) {
@@ -32,6 +42,7 @@ export const useMarketInfos = (contractAddr: string): Market | undefined => {
       userNoBet: ethers.utils.formatEther(userNo),
       yesTokenTotSupply: ethers.utils.formatEther(yesTot),
       noTokenTotSupply: ethers.utils.formatEther(noTot),
+      volume: volume,
     };
 
     setMarket(market);
